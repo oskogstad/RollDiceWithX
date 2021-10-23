ae926c71e8dd10b5d85ec38d484f73492de637b5
istarnion <hallgeir@loekken.org>
2021-10-23 21:57:48 +0200

    Sketch basic JS flow

--- static/room.js
+++ static/room.js
@@ -1,5 +1,53 @@
 const path = window.location.pathname.split("/");
 const roomname = path[path.length - 1];
+const username = "tempuser";
+const rollinput = document.getElementById("roll-input");
+const rolllist = document.getElementById("roll-lsit");
 
 document.title = "Roll Dice With " + roomname;
+
+const connection = new SignalR.HubConnectionBuilder()
+    .withUrl("/roomhub")
+    .configureLogging(signalR.LogLevel.Information)
+    .withAutomaticReconnect()
+    .build();
+
+async function start_signalr() {
+    try {
+        await connection.start();
+        console.log("SignalR connected");
+        await connection.invoke("JoinRoom", roomname);
+    }
+    catch(err) {
+        console.error(err);
+    }
+}
+
+connection.on("RoomJoined", () => {
+    console.log("Joined", roomname);
+});
+
+connection.on("PublishRoll", (message) => {
+    console.log("PublishRoll", user, message);
+    const li = document.createElement("li");
+    li.textContent = "PublishRoll " + " " + message;
+    rolllist.appendChild(li);
+});
+
+start_signalr();
+
+async function roll(expr) {
+    try {
+        await connection.invoke("Roll", roomname, username, expr);
+    }
+    catch(err) {
+        console.error(err);
+    }
+}
+
+function submit_roll() {
+    const expr = rollinput.value;
+    roll(expr);
+    return false;
+}
 
