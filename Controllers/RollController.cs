c3543e3809562bb414531c8b95526454d761a106
Ole Jørgen Skogstad <skogstad@softis.net>
2021-10-23 20:26:40 +0200

    Add simple roll controller, use dicebag

---
+++ Controllers/RollController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Result = Dicebag.DiceRoller.Result;
+
+namespace RollDiceWithX.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RollController : ControllerBase
+    {
+        private readonly ILogger<RollController> _logger;
+
+        public RollController(ILogger<RollController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Result Get([FromQuery] string exp)
+        {
+            _logger.LogInformation($"Rolling expression '{exp}'");
+            return Dicebag.DiceRoller.Roll(exp);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -12,10 +12,9 @@ public class Startup
         Configuration = configuration;
     }
 
-    public IConfiguration Configuration { get; }
+    private IConfiguration Configuration { get; }
 
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
+    public static void ConfigureServices(IServiceCollection services)
     {
         services.AddControllers();
         services.AddSwaggerGen(c =>
@@ -24,8 +23,7 @@ public class Startup
         });
     }
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
         if (env.IsDevelopment())
         {
