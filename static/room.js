3ce8193884fec8e327b70bbbe06b3b152cf8cb5c
Ole Jørgen Skogstad <skogstad@softis.net>
2021-10-23 22:25:09 +0200

    Fix Hallgeirs errors

--- static/room.js
+++ static/room.js
@@ -2,11 +2,11 @@ const path = window.location.pathname.split("/");
 const roomname = path[path.length - 1];
 const username = "tempuser";
 const rollinput = document.getElementById("roll-input");
-const rolllist = document.getElementById("roll-lsit");
+const rolllist = document.getElementById("roll-list");
 
 document.title = "Roll Dice With " + roomname;
 
-const connection = new SignalR.HubConnectionBuilder()
+const connection = new signalR.HubConnectionBuilder()
     .withUrl("/roomhub")
     .configureLogging(signalR.LogLevel.Information)
     .withAutomaticReconnect()
@@ -28,7 +28,7 @@ connection.on("RoomJoined", () => {
 });
 
 connection.on("PublishRoll", (message) => {
-    console.log("PublishRoll", user, message);
+    console.log("PublishRoll", message);
     const li = document.createElement("li");
     li.textContent = "PublishRoll " + " " + message;
     rolllist.appendChild(li);
@@ -43,6 +43,7 @@ async function roll(expr) {
     catch(err) {
         console.error(err);
     }
+    // return true;
 }
 
 function submit_roll() {
