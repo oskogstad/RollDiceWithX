9304268c96cf8c7842c77d192afc5d5d03f32442
Ole Jørgen Skogstad <skogstad@softis.net>
2021-11-21 20:13:40 +0100

    Disable serviceworker

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -4,8 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import registerServiceWorker from './registerServiceWorker';
+// import reportWebVitals from './reportWebVitals';
+// import registerServiceWorker from './registerServiceWorker';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
@@ -18,9 +18,9 @@ ReactDOM.render(
     </React.StrictMode>,
     rootElement);
 
-registerServiceWorker();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// registerServiceWorker();
+//
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
