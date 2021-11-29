32945050fe35d7f5b981be48c5241b95dac19dc1
Ole Jørgen Skogstad <skogstad@softis.net>
2021-11-29 21:59:25 +0100

    Make input form and roll details responsive

--- ClientApp/src/components/RollInput.js
+++ ClientApp/src/components/RollInput.js
@@ -4,15 +4,15 @@ import {useState} from "react";
 const RollInput = ({sendRoll}) => {
     const [rollExpression, setRollExpression] = useState();
     
-    return <Form className="roll-input-form" onSubmit={event => {
+    return <Form className="roll-input-form backgroundTestPink" onSubmit={event => {
         event.preventDefault();
         sendRoll(rollExpression);
     }}>
-        <Form.Group>
-            <Form.Control placeholder="e.g. 2d6+2d8+10" 
+            <Form.Group className="">
+                <Form.Control placeholder="e.g. 2d6+2d8+10" 
                           onChange={
                               event => setRollExpression(event.target.value)} />
-        </Form.Group>
+            </Form.Group>
         <Button type="submit" disabled={!rollExpression}>Roll!</Button>
     </Form>
 }
